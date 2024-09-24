32730569cdce0f9e3d867002bd73a5971e81833a
Agustin Bustamante <agusbusta10@gmail.com>
2024-09-24 18:13:30 -0300

    6p

--- ecommerce/src/components/CartItem/CartItem.jsx
+++ ecommerce/src/components/CartItem/CartItem.jsx
@@ -1,27 +1,26 @@
 import './CartItem.css';
+import { CartContext } from '../../context/CartContext.jsx'
+import { useContext } from 'react';
 
 const CartItem = ({ id, name, img, category, description, price, stock }) => {
 
+    const { cart, addItem } = useContext(CartContext);
+    const item = cart.find(item => item.id === id);
 
 
     return (
         <div className='cart-container'>
             <article className="arti-contenedor">
-                <picture>
+                <picture className='box'>
                     <img src={img} alt={name} className="cart-img" />
-                    x CANTIDAD
                 </picture>
                 <header className="">
-                    <h2 className=""> {name} </h2>
+                    <h3 className=""> {name} </h3>
                 </header>
-                <section>
-                    <p className="">
-                        Categoria: {category}
-                    </p>
-                    <p className="">
-                        Price: ${price}
-                    </p>
-                </section>
+                <p className="">
+                    ${price} x {item ? item.quantity : 1} =
+                </p>
+                <h3>$ {price * (item ? item.quantity : 1)}</h3>
             </article>
         </div>
     )
